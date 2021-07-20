ffbc9664c789e1cb7f39a922804394c9f1151e96
Adriana-Gabrielyan <adrin.gabrielyan1997@gmail.com>
2021-07-20 12:42:35 +0400

    add button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { ButtonComponent } from './components/button/button.component';
 @NgModule({
-  declarations: [AppComponent, CardComponent, ModalComponent],
+  declarations: [AppComponent, CardComponent, ModalComponent, ButtonComponent],
   imports: [BrowserModule],
   entryComponents: [CardComponent],
   providers: [],
